d537f115724f0ce02e2451582dbf07641363fb62
trinith01 <162677526+trinith01@users.noreply.github.com>
2025-08-14 10:35:26 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import { API_BASE_URL } from "@/config/apiConfig";
 
 const api = axios.create({
   //  baseURL: API_BASE_URL,
-  baseURL: "http://localhost:5000",
+  baseURL: "https://matrimony-mrq1.onrender.com",
 });
 
 export default api;
